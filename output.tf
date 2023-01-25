96519f46d3f48e038e8ba80ae8bfbad7f24294b4
pkstiyara <pkstiyara@gmail.com>
2023-01-25 08:56:21 +0530

    adding nodes

---
+++ output.tf
@@ -0,0 +1,12 @@
+
+
+# output "Jenkins_ip" {
+#   value = aws_instance.jenkins.public_ip
+# }
+# output "sonar_ip" {
+#   value = aws_instance.sonar.public_ip
+# }
+# output "nuxus_ip" {
+#   value = aws_instance.nexus.public_ip
+# }
+
